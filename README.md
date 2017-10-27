31c0d2e897a6916cd8d406c4683afbe6cb9fe4e9
Matthieu Lemoine <mlemoine@yumii.fr>
2017-10-27 14:36:22 +0200

    :sparkles: Add a constants export

--- README.md
+++ README.md
@@ -49,7 +49,7 @@ import {
   NOTIFICATION_SERVICE_ERROR,
   NOTIFICATION_RECEIVED as ON_NOTIFICATION_RECEIVED,
   TOKEN_UPDATED,
-} from 'electron-push-receiver';
+} from 'electron-push-receiver/constants';
 
 // Listen for service successfully started
 ipcRenderer.on(NOTIFICATION_SERVICE_STARTED, (_, token) => // do something);
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 const { register, listen } = require('push-receiver');
 const { ipcMain } = require('electron');
 const Config = require('electron-config');
-
-// Event to be sent from renderer process to trigger service start
-const START_NOTIFICATION_SERVICE = 'PUSH_RECEIVER:::START_NOTIFICATION_SERVICE';
-// Event sent to the renderer process once the service is up
-const NOTIFICATION_SERVICE_STARTED = 'PUSH_RECEIVER:::NOTIFICATION_SERVICE_STARTED';
-// Event sent to the renderer process if an error has occured during the starting process
-const NOTIFICATION_SERVICE_ERROR = 'PUSH_RECEIVER:::NOTIFICATION_SERVICE_ERROR';
-// Event sent to the renderer processs when a notification has been received
-const NOTIFICATION_RECEIVED = 'PUSH_RECEIVER:::NOTIFICATION_RECEIVED';
-// Event sent to the renderer processs when the FCM token has been updated
-const TOKEN_UPDATED = 'PUSH_RECEIVER:::TOKEN_UPDATED';
+const {
+  START_NOTIFICATION_SERVICE,
+  NOTIFICATION_SERVICE_STARTED,
+  NOTIFICATION_SERVICE_ERROR,
+  NOTIFICATION_RECEIVED,
+  TOKEN_UPDATED,
+} = require('./constants');
 
 const config = new Config();
 
