016b133af13d99162b235dfcd6b9793d48a4be07
PomepuyN <kh3p3ra@gmail.com>
2018-04-18 09:56:37 +0200

    Add type definitions

---
+++ src/electron-push-receiver.d.ts
@@ -0,0 +1,11 @@
+interface ElectronPushReceiver {
+    START_NOTIFICATION_SERVICE: string;
+    NOTIFICATION_SERVICE_STARTED: string;
+    NOTIFICATION_SERVICE_ERROR: string;
+    NOTIFICATION_RECEIVED: string;
+    TOKEN_UPDATED: string;
+    setup: (webContents: Electron.WebContents) => void;
+}
+
+declare const electronPushReceiver: ElectronPushReceiver;
+export = electronPushReceiver;
